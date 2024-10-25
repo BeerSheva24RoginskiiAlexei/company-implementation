5417e0df2d2105dd9cae49b985220f60116d7b6c
AlexeiRoginskii <roginskiial@gmail.com>
2024-10-25 19:57:07 +0300

    add a HW

--- src/main/java/telran/employees/CompanyImpl.java
+++ src/main/java/telran/employees/CompanyImpl.java
@@ -1,111 +1,169 @@
 package telran.employees;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.TreeMap;
 
 import telran.io.Persistable;
 
-public class CompanyImpl implements Company, Persistable{
-   private TreeMap<Long, Employee> employees = new TreeMap<>();
-   private HashMap<String, List<Employee>> employeesDepartment = new HashMap<>();
-   private TreeMap<Float, List<Manager>> managersFactor = new TreeMap<>();
-private class CompanyIterator implements Iterator<Employee> {
-    Iterator<Employee> iterator = employees.values().iterator();
-    Employee lastIterated;
-    @Override
-    public boolean hasNext() {
-       return iterator.hasNext();
+public class CompanyImpl implements Company, Persistable {
+    private TreeMap<Long, Employee> employees = new TreeMap<>();
+    private HashMap<String, List<Employee>> departments = new HashMap<>();
+    private TreeMap<Float, List<Manager>> managersFactor = new TreeMap<>();
+
+    private class CompanyImplIterator implements Iterator<Employee> {
+        private Iterator<Employee> iterator = employees.values().iterator();
+        private Employee prev = null;
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Employee next() {
+            return prev = iterator.next();
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+            removeDepartment(prev);
+            removeManager(prev);
+        }
     }
 
-    @Override
-    public Employee next() {
-       lastIterated = iterator.next();
-       return lastIterated;
-    }
-    @Override
-    public void remove() {
-       iterator.remove();
-       removeFromIndexMaps(lastIterated);
-    }
-}
     @Override
     public Iterator<Employee> iterator() {
-       return new CompanyIterator();
+        return new CompanyImplIterator();
     }
 
+    /**
+     * Time complexity: O(log n).
+     */
     @Override
     public void addEmployee(Employee empl) {
-        long id = empl.getId();
-        if (employees.putIfAbsent(id, empl) != null) {
-            throw new IllegalStateException("Already exists employee " + id);
+        Employee oldEmployee = employees.putIfAbsent(empl.getId(), empl);
+        if (oldEmployee != null) {
+            throw new IllegalStateException();
         }
-        addIndexMaps(empl);
+
+        addDepartment(empl);
+        addManager(empl);
     }
 
-    private void addIndexMaps(Employee empl) {
-       employeesDepartment.computeIfAbsent(empl.getDepartment(), k -> new ArrayList<>()).add(empl);
-       if (empl instanceof Manager manager) {
-            managersFactor.computeIfAbsent(manager.getFactor(), k -> new ArrayList<>()).add(manager);
-       }
+    /**
+     * Time complexity: O(log n)
+     */
+    private void addManager(Employee empl) {
+        if (empl instanceof Manager manager) {
+            managersFactor.computeIfAbsent(manager.getFactor(), i -> new ArrayList<>()).add(manager);
+        }
     }
 
-    
+    /**
+     * Time complexity: O(1)
+     */
+    private void addDepartment(Employee empl) {
+        String department = empl.getDepartment();
 
+        if (department != null) {
+            departments.computeIfAbsent(department, i -> new ArrayList<>()).add(empl);
+        }
+    }
+
+    /**
+     * Time complexity: O(log n)
+     */
     @Override
     public Employee getEmployee(long id) {
         return employees.get(id);
     }
 
+    /**
+     * Time complexity: O(log n)
+     */
     @Override
     public Employee removeEmployee(long id) {
-        Employee empl = employees.remove(id);
-        if(empl == null) {
-            throw new NoSuchElementException("Not found employee " + id);
+        Employee removedEmpl = employees.remove(id);
+
+        if (removedEmpl == null) {
+            throw new NoSuchElementException();
         }
-        removeFromIndexMaps(empl);
-        return empl;
+
+        removeDepartment(removedEmpl);
+        removeManager(removedEmpl);
+
+        return removedEmpl;
     }
 
-
-    private void removeFromIndexMaps(Employee empl) {
-        removeIndexMap(empl.getDepartment(), employeesDepartment, empl);
+    /**
+     * Time complexity: O(log n)
+     */
+    private void removeManager(Employee empl) {
         if (empl instanceof Manager manager) {
-            removeIndexMap(manager.getFactor(), managersFactor, manager);
+            Float factor = manager.getFactor();
+            List<Manager> managers = managersFactor.get(factor);
+            managers.remove(manager);
+
+            if (managers.isEmpty()) {
+                managersFactor.remove(factor);
+            }
         }
     }
 
-    private <K, V extends Employee> void removeIndexMap(K key, Map<K, List<V>> map, V empl) {
-        List<V> list = map.get(key);
-        list.remove(empl);
-        if (list.isEmpty()) {
-            map.remove(key);
+    /**
+     * Time complexity: O(1)
+     */
+    private void removeDepartment(Employee empl) {
+        String department = empl.getDepartment();
+        if (department != null) {
+            List<Employee> employees = departments.get(department);
+            employees.remove(empl);
+
+            if (employees.isEmpty()) {
+                departments.remove(department);
+            }
         }
     }
 
     @Override
     public int getDepartmentBudget(String department) {
-        return employeesDepartment.getOrDefault(department, Collections.emptyList())
-        .stream().mapToInt(Employee::computeSalary).sum();
+        int sum = 0;
+        List<Employee> employees = departments.get(department);
+
+        if (employees != null) {
+            sum = employees.stream().mapToInt(i -> i.computeSalary()).sum();
+        }
+        return sum;
     }
 
     @Override
     public String[] getDepartments() {
-        return employeesDepartment.keySet().stream().sorted().toArray(String[]::new);
+        return departments.keySet().stream().sorted().toArray(String[]::new);
     }
 
+    /**
+     * Time complexity: O(1)
+     */
     @Override
     public Manager[] getManagersWithMostFactor() {
-        Manager [] res = new Manager[0];
+        Manager[] res = new Manager[0];
         if (!managersFactor.isEmpty()) {
-            res = managersFactor.lastEntry().getValue().toArray(res);
+            res = managersFactor.lastEntry().getValue().toArray(new Manager[0]);
         }
         return res;
     }
 
     @Override
     public void saveToFile(String fileName) {
-        try (PrintWriter writer = new PrintWriter(fileName)) {
+        try (PrintWriter writer = new PrintWriter(fileName);) {
             forEach(writer::println);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -114,12 +172,10 @@ private class CompanyIterator implements Iterator<Employee> {
 
     @Override
     public void restoreFromFile(String fileName) {
-        try (BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
-            reader.lines().map(Employee::getEmployeeFromJSON).forEach(this::addEmployee);
-        } catch (FileNotFoundException e) { 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            reader.lines().forEach(i -> addEmployee(Employee.getEmployee(i)));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
 }
--- src/main/java/telran/employees/Employee.java
+++ src/main/java/telran/employees/Employee.java
@@ -4,61 +4,75 @@ import org.json.JSONObject;
 
 public class Employee {
     private long id;
-    private int basicSalary;
+    private int salary;
     private String department;
-    public Employee(){
 
-    }
-    @SuppressWarnings("unchecked")
-    static public Employee getEmployeeFromJSON(String jsonStr) {
-        JSONObject jsonObj = new JSONObject(jsonStr);
-        String className = jsonObj.getString("className");
+    static public Employee getEmployee(String jsonStr) {
+        JSONObject json = new JSONObject(jsonStr);
+        String className = json.getString("className");
+
         try {
             Class<Employee> clazz = (Class<Employee>) Class.forName(className);
-            Employee empl =  clazz.getConstructor().newInstance();
-            empl.setObject(jsonObj);
-            return empl;
+            Employee employee = clazz.getConstructor(null).newInstance();
+            employee.setObject(json);
+            return employee;
         } catch (Exception e) {
-           throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
+
     }
-   protected void setObject(JSONObject jsonObj) {
-       id = jsonObj.getLong("id");
-       basicSalary = jsonObj.getInt("basicSalary");
-       department = jsonObj.getString("department");
+
+    public Employee() {
     }
-    public Employee(long id, int basicSalary, String department) {
+
+    public Employee(long id, int salary, String department) {
         this.id = id;
-        this.basicSalary = basicSalary;
+        this.salary = salary;
         this.department = department;
     }
-    public int computeSalary() {
-        return basicSalary;
-    }
+
     public long getId() {
         return id;
     }
+
+    public int getSalary() {
+        return salary;
+    }
+
     public String getDepartment() {
         return department;
     }
+
+    public int computeSalary() {
+        return salary;
+    }
+
     @Override
     public boolean equals(Object obj) {
-       boolean res = false;
-       if (obj instanceof Employee empl) {
-            res = id == empl.id;
-       }
-       return res;
+        boolean res = false;
+        if (obj instanceof Employee empl) {
+            res = id == empl.getId();
+        }
+        return res;
     }
+
     @Override
     public String toString() {
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("className", getClass().getName());
-        fillJSON(jsonObj);
-        return jsonObj.toString();
+        JSONObject json = new JSONObject();
+        json.put("className", getClass().getName());
+        fillJSON(json);
+        return json.toString();
     }
-   protected void fillJSON(JSONObject jsonObj) {
-        jsonObj.put("id",id);
-        jsonObj.put("basicSalary", basicSalary);
-        jsonObj.put("department", department);
+
+    protected void fillJSON(JSONObject json) {
+        json.put("id", id);
+        json.put("salary", salary);
+        json.put("department", department);
+    }
+
+    protected void setObject(JSONObject json) {
+        id = json.getLong("id");
+        salary = json.getInt("salary");
+        department = json.getString("department");
     }
 }
